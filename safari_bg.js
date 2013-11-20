a647ca8338a3c0f42a439d6db17b0143d996eba8
Michael Gundlach <gundlach@getadblock.com>
2013-11-20 14:07:56 -0500

    Correction: apparently function foo() creates a local!

--- safari_bg.js
+++ safari_bg.js
@@ -91,7 +91,7 @@ if (!LEGACY_SAFARI) {
   (function() {
     // Added this function so that safari will have it's own way of retrieving custom filters.
     // Return filters created by user in an array.
-    function getCustomFilters() {
+    getCustomFilters = function() {
       var custom_filters = storage_get('custom_filters');
       return custom_filters ? custom_filters.trimLeft() : '';
     };
