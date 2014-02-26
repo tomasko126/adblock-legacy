2e00e682a2d4a21fcc6d13d85b216b326d9c0081
Tomáš Taro <tomasko126@gmail.com>
2014-02-26 22:32:58 +0100

    Various changes
    - Fixed bug, when YouTube() wasn’t called after clicking on video from
    homepage
    - Added check preventing adding channel name, if channel name is added
    in URL

--- unblockyoutube.js
+++ unblockyoutube.js
@@ -1,20 +1,22 @@
 // Disable HTML5 History API on YouTube
 document.location.href = "javascript:void(history.pushState = undefined);";
 
-// Initialize YouTube whitelist function
-document.addEventListener("DOMNodeInserted", function() {
-  var url = window.location.search;
-  if (url.search("channel=") == -1) {
-   YouTube();
-  }
-},true);
- 
+// It is unnecessary to run it on main page
+var oldLocation = "http://www.youtube.com/";
+if (document.location.href != oldLocation) {
+ window.onload = function() {
+  YouTube();
+ }
+}
 
 function YouTube() {
  var getytname = document.getElementsByClassName("yt-user-name")[0].innerText || document.getElementsByClassName("yt-user-name")[1].innerText;
  var url = window.location.search;
  var putintoit = url+"&channel="+getytname;
- window.history.replaceState(null,null,putintoit);
- var location = document.location.href;
- document.location.href = location;
+  if (url.search("channel=") > 0) {
+   return;
+  } else {
+   window.history.replaceState(null,null,putintoit);
+   document.location.reload();
+  }
 };
