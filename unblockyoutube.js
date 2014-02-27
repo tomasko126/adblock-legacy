0f794116159aeb81da2b661eb2f9a7cab696d5c2
Tomáš Taro <tomasko126@gmail.com>
2014-02-27 23:22:12 +0100

    Various changes

--- unblockyoutube.js
+++ unblockyoutube.js
@@ -1,31 +1,35 @@
 // Disable HTML5 History API on YouTube
 window.onload = function() {
-document.location.href = "javascript:void(history.pushState = undefined);";
+ document.location.href = "javascript:void(history.pushState = undefined);";
 }
 
 // Don't run on main page and search page
 var unsecure = "http://www.youtube.com/";
 var secure = "https://www.youtube.com/";
 if (document.location.href != unsecure && document.location.href != secure && document.location.href.search("feed") <  0  && document.location.href.search("search") == -1) {
-  YouTube();
+ YouTube();
 }
 
 // Main function, which finds name of the channel and
 // put it at the end of URL like &channel=xyz where xyz
 // is name of the channel, so we are able to whitelist
 // channel easily
-var putintoit;
+var new_url, get_yt_name;
 
 function YouTube() {
- var getytname = document.getElementsByClassName("epic-nav-item-heading")[0].innerText || document.getElementsByClassName("yt-user-name")[0].innerText || document.getElementsByClassName("yt-user-name")[1].innerText;
  var url = window.location.href;
-  if (url.search("user") > 0 || url.search("/channel/") > 0) {
-   putintoit = url+"?&channel="+getytname;
+  if (url.search("user") > 0) {
+   get_yt_name = document.getElementsByClassName("epic-nav-item-heading")[0].innerText;
   } else {
-   putintoit = url+"&channel="+getytname;
+   get_yt_name = document.getElementsByClassName("yt-user-name")[0].innerText || document.getElementsByClassName("yt-user-name")[1].innerText;
+  }
+  if (url.search("user") > 0 || url.search("/channel/") > 0) {
+   new_url = url+"?&channel="+get_yt_name;
+  } else {
+   new_url = url+"&channel="+get_yt_name;
   }
   if (url.search("channel=") < 0) {
-   window.history.replaceState(null,null,putintoit);
+   window.history.replaceState(null,null,new_url);
    // Page must be reloaded, so AdBlock can properly whitelist a video
    document.location.reload();
   }
