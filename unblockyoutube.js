19d8f7ceb3078b48d5addc152652d58977dcd2ba
Tomáš Taro <tomasko126@gmail.com>
2014-02-28 15:31:01 +0100

    Various changes
    - Edited comments
    - Variables are local now

--- unblockyoutube.js
+++ unblockyoutube.js
@@ -1,15 +1,16 @@
-// Disable HTML5 History API on YouTube
+// Disable history.pushState() on YouTube in Chrome and Opera,
+// in Safari is history.pushState() disabled in bandaids.js
 if (!SAFARI) {
 window.onload = function() {
   document.location.href = "javascript:void(history.pushState = undefined);";
  }
 }
 
-// Don't run on main page and search page
+// Don't run on main, search and feed page
 var unsecure = "http://www.youtube.com/";
 var secure = "https://www.youtube.com/";
 var address = document.location.href;
-if (address != unsecure && address != secure && address.search("feed") < 0 && address.search("search") == -1) {
+if (address != unsecure && address != secure && address.search("feed") < 0 && address.search("search") < 0) {
  YouTube();
 }
 
@@ -17,16 +18,14 @@ if (address != unsecure && address != secure && address.search("feed") < 0 && ad
 // put it at the end of URL like &channel=xyz where xyz
 // is name of the channel, so we are able to whitelist
 // channel easily
-var new_url, get_yt_name;
-
 function YouTube() {
  var url = window.location.href;
   if (url.search("user") > 0 || url.search("/channel") > 0) {
-   get_yt_name = document.getElementsByClassName("epic-nav-item-heading")[0].innerText;
-   new_url = url+"?&channel="+get_yt_name;
+   var get_yt_name = document.getElementsByClassName("epic-nav-item-heading")[0].innerText;
+   var new_url = url+"?&channel="+get_yt_name;
   } else {
-   get_yt_name = document.getElementsByClassName("yt-user-name")[0].innerText || document.getElementsByClassName("yt-user-name")[1].innerText;
-   new_url = url+"&channel="+get_yt_name;
+   var get_yt_name = document.getElementsByClassName("yt-user-name")[0].innerText || document.getElementsByClassName("yt-user-name")[1].innerText;
+   var new_url = url+"&channel="+get_yt_name;
   }
   if (url.search("channel=") < 0) {
    window.history.replaceState(null,null,new_url);
