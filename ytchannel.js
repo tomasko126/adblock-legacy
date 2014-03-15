b1780ededfcffde24cd884e5ab06e50d48885968
Tomáš Taro <tomasko126@gmail.com>
2014-03-15 17:48:54 +0100

    Various changes
    Now, History.pushState is disabled in Safari properly, so thumbnails
    and comments are visible again.
    
    When user clicks on another video, body will be deleted so user won’t
    see in some cases the page which is loading twice.
    
    Comments were added to describe functionality.
    
    Closes #140

--- ytchannel.js
+++ ytchannel.js
@@ -1,19 +1,37 @@
 // Run just on YouTube
 if (/youtube/.test(document.location.hostname)) {
-  // Disable history.pushState() in Chrome and Opera,
-  // so we won't have problems with navigation and running this code.
-  // For Safari see bandaids.js
+  // If history.pushState is available, YouTube uses the history API
+  // when navigation from one video to another, and tells the flash player with JavaScript
+  // which video and which ads to show next, bypassing our flashvars rewrite code.
+  // So we disable history.pushState on pages with YouTube's flash player,
+  // this will for the site to use to a page reload.
   if (!SAFARI) {
     window.onload = function() {
       document.location.href = "javascript:void(history.pushState = undefined);";
     }
+  // Onload event is fired differently in Safari,
+  // so we have to check whether the page has been completely loaded
+  // or not by using readyState event
+  } else {
+    var _timer=setInterval(function() {
+      if (/loaded|complete/.test(document.readyState)) {
+        clearInterval(_timer);
+        document.location.href = "javascript:void(history.pushState = undefined);";
+      }
+    }, 250)
+  }
+
+  // Remove body when YouTube is loading another video,
+  // so we don't see the same page twice in some cases
+  window.onbeforeunload = function() {
+    document.body.parentNode.removeChild(document.body);
   }
 
   // Don't run on main, search and feed page
   var unsecure = "http://www.youtube.com/";
   var secure = "https://www.youtube.com/";
   var address = document.location.href;
-  if (address != unsecure && address != secure && address.search("feed") < 0 && address.search("search") < 0) {
+  if (address !== unsecure && address !== secure && address.search("feed") < 0 && address.search("search") < 0) {
     changeURL();
   }
 
@@ -36,7 +54,7 @@ if (/youtube/.test(document.location.hostname)) {
       // Add the name of the channel to the end of URL
       window.history.replaceState(null,null,new_url);
       // Page must be reloaded, so AdBlock can properly whitelist the page
-      location.reload();
+      document.location.reload();
     }
   }
 }
