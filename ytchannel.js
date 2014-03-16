e3bb71c07e1e2d6ec6f1b18608bc93d4699fdc75
Tomáš Taro <tomasko126@gmail.com>
2014-03-16 19:15:02 +0100

    Various changes
    - Changed name of the variable
    - Test content of the URL with another method
    - Minor improvements

--- ytchannel.js
+++ ytchannel.js
@@ -13,9 +13,9 @@ if (/youtube/.test(document.location.hostname)) {
   // so we have to check whether the page has been completely loaded
   // or not by using readyState event
   } else {
-    var _timer=setInterval(function() {
+    var disable_history_api = setInterval(function() {
       if (/loaded|complete/.test(document.readyState)) {
-        clearInterval(_timer);
+        clearInterval(disable_history_api);
         document.location.href = "javascript:void(history.pushState = undefined);";
       }
     }, 50)
@@ -28,23 +28,19 @@ if (/youtube/.test(document.location.hostname)) {
   }
 
   // Don't run on main, search and feed page
-  var unsecure = "http://www.youtube.com/";
-  var secure = "https://www.youtube.com/";
   var address = document.location.href;
-  if (address !== unsecure && address !== secure && address.search("feed") < 0 && address.search("search") < 0) {
+  if ((/user|watch|channel/.test(document.location.href)) && address.search("feed") < 0 && address.search("&channel") < 0) {
     changeURL();
-    if (address.search("channel") < 1) {
-      document.body.style.display = "none";
-    }
   }
 
   // Main function, which finds name of the channel and puts it at the end of the URL.
   // On the end will be "&channel=xyz", where "xyz" is a name of the channel,
   // so we are able to whitelist the YouTube channel with the easiest way
   function changeURL() {
+    document.body.style.display = "none";
     var url = document.location.href;
     // Grab name of the channel
-    if (url.search("user") > 0 || url.search("/channel") > 0) {
+    if (/user|channel/.test(document.location.href)) {
       var get_yt_name = document.getElementsByClassName("epic-nav-item-heading")[0].innerText;
       var extracted_name = get_yt_name.replace(/\s/g, '');
       var new_url = url+"?&channel="+extracted_name;
@@ -55,13 +51,11 @@ if (/youtube/.test(document.location.hostname)) {
         var new_url = url+"&channel="+extracted_name;
       } catch (e) {} // Silently fail
     }
-    if (url.search("channel=") < 0) {
       // We hide body of the page, so user won't see reloading of the page
       document.body.style.display = "none";
       // Add the name of the channel to the end of URL
       window.history.replaceState(null,null,new_url);
       // Page must be reloaded, so AdBlock can properly whitelist the page
-      document.location.reload();
-    }
+      location.reload(true);
   }
 }
