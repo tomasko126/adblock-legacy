9aaa83e40a903fc9732e1e1020a17b6d588a6cf0
Tomáš Taro <tomasko126@gmail.com>
2014-06-16 23:40:15 +0200

    Use XHR call for obtaining content
    Put content of the changelog file into textarea

--- options/support.js
+++ options/support.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+    // Check, whether update is available
     $("#checkupdate").html(translate("checkforupdates"));
     checkupdates("help");
-    $("#changelog").attr('href', SAFARI ? safari.extension.baseURI + "CHANGELOG.txt": document.location.origin+"/CHANGELOG.txt");
+    
+    // Show the changelog
+    $("#whatsnew a").click(function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", chrome.extension.getURL("CHANGELOG"), false);
+        xhr.send();
+        var object = xhr.responseText;
+        $("textarea").html(object).css({width: "670px", height: "200px"}).fadeIn();
+    });
 });
