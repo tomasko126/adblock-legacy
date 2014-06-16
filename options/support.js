943d8692adedc84d31fa22000c96def5594e9ee8
Tomáš Taro <tomasko126@gmail.com>
2014-06-16 21:51:00 +0200

    Implement link for changelog
    I had to rename CHANGELOG to CHANGELOG.txt, because Safari has always
    crashed when accessing CHANGELOG file.

--- options/support.js
+++ options/support.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
-  $("#checkupdate").html(translate("checkforupdates"));
-  checkupdates("help");
+    $("#checkupdate").html(translate("checkforupdates"));
+    checkupdates("help");
+    $("#changelog").attr('href', SAFARI ? safari.extension.baseURI + "CHANGELOG.txt": document.location.origin+"/CHANGELOG.txt");
 });
