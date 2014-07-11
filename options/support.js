b3c660ac0d4e39c2a021ae1fd1c3fa9adcc38b55
Tomáš Taro <tomas@getadblock.com>
2014-07-11 21:18:59 +0200

    Changelog code was accidentally removed

--- options/support.js
+++ options/support.js
@@ -113,4 +113,13 @@ $(document).ready(function() {
         "?category_id=problems&discussion[body]=" + out;
         document.location.href = result; 
     });
+    
+    // Show the changelog
+    $("#whatsnew a").click(function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", chrome.extension.getURL("CHANGELOG.txt"), false);
+        xhr.send();
+        var object = xhr.responseText;
+        $("#changes").text(object).css({width: "670px", height: "200px"}).fadeIn();
+    });
 });
