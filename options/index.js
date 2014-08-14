54f63d7a73dfadd2cc178512acc1e188785301b7
Tomáš Taro <tomas@getadblock.com>
2014-08-15 01:29:58 +0200

    Should fix an issue with translators names on Safari

--- options/index.js
+++ options/index.js
@@ -105,12 +105,39 @@ function displayTranslationCredit() {
         xhr.open("GET", chrome.extension.getURL('translators.json'), true);
         xhr.onload = function() {
             var text = JSON.parse(this.responseText);
-            for (var id in text)
-                if (id === navigator.language.substring(0, 2))
-                    for (var translator in text[id].translators) {
-                        var name = text[id].translators[translator].credit;
-                        translators.push(" " + name);
+            var lang = navigator.language;
+            for (var id in text) {
+                if (!SAFARI) {
+                    if (id === navigator.language) {
+                        for (var translator in text[id].translators) {
+                            var name = text[id].translators[translator].credit;
+                            translators.push(" " + name);
+                        }
+                    } else {
+                       for (var translator in text[id].translators) {
+                          var lang = navigator.language.toLowerCase();
+                          if (lang === id) {
+                              var name = text[lang].translators[translator].credit;
+                              translators.push(" " + name);
+                          }
+                       } 
                     }
+                } else {
+                    if (lang.substring(0, 2) === id) {
+                        for (var translator in text[id].translators) {
+                            var name = text[id].translators[translator].credit;
+                            translators.push(" " + name);
+                        }
+                    } else {
+                      for (var translator in text[id].translators) {
+                          if (lang === id) {
+                            var name = text[lang].translators[translator].credit;
+                            translators.push(" " + name);
+                          }
+                        }
+                    }
+                }
+            }
             $("#translator_credit").text(translate("translator_credit"));
             $("#translator_names").text(translators.toString());
         }
