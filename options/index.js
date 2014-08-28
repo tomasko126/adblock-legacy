f5026c75a65a8e2050e27e813cb9a1b1dc6ae870
Tomáš Taro <tomas@getadblock.com>
2014-08-29 00:44:34 +0200

    Cache tabs

--- options/index.js
+++ options/index.js
@@ -1,31 +1,42 @@
 function load_options() {
-  // Check or uncheck each option.
-  BGcall("get_settings", function(settings) {
-    optionalSettings = settings;
-    $("#tabpages").
-      tabs({
-        load: function(event, ui) {
-          //translation
-          localizePage();
+    // Check or uncheck each option.
+    BGcall("get_settings", function(settings) {
+        optionalSettings = settings;
+        $("#tabpages").
+        tabs({
+            // Cache tabs
+            beforeLoad: function(event, ui) {
+                if (ui.tab.data("loaded")) {
+                    event.preventDefault();
+                    return;
+                }
+ 
+                ui.jqXHR.success(function() {
+                    ui.tab.data("loaded", true);
+                });
+            },
+            load: function(event, ui) {
+                //translation
+                localizePage();
 
-          // Toggle won't handle .advanced.chrome-only
-          if (!optionalSettings.show_advanced_options)
-            $(".advanced").hide();
-          if (SAFARI)
-            $(".chrome-only").hide();
+                // Toggle won't handle .advanced.chrome-only
+                if (!optionalSettings.show_advanced_options)
+                    $(".advanced").hide();
+                if (SAFARI)
+                    $(".chrome-only").hide();
 
-          // Must load tab .js here: CSP won't let injected html inject <script>
-          // see index.html:data-scripts
-          ui.tab["0"].dataset.scripts.split(' ').forEach(function(scriptToLoad) {
-            // CSP blocks eval, which $().append(scriptTag) uses
-            var s = document.createElement("script");
-            s.src = scriptToLoad;
-            document.body.appendChild(s);
-          });
-        },
-      }).
-      show();
-  });
+                // Must load tab .js here: CSP won't let injected html inject <script>
+                // see index.html:data-scripts
+                ui.tab["0"].dataset.scripts.split(' ').forEach(function(scriptToLoad) {
+                    // CSP blocks eval, which $().append(scriptTag) uses
+                    var s = document.createElement("script");
+                    s.src = scriptToLoad;
+                    document.body.appendChild(s);
+                });
+            },
+        }).
+        show();
+    });
 }
 
 var language = navigator.language.match(/^[a-z]+/i)[0];
