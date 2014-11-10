fd1b0571a8b387aa6fb3265c59d5d787498b2566
Brent Montrose <brent@getadblock.com>
2014-11-10 16:30:20 -0600

    updated the URL to check for, and add a check for no yt channle name

--- background.js
+++ background.js
@@ -874,13 +874,15 @@
   // Inputs: url:string url of the page
   // Returns: null if successful, otherwise an exception
   create_whitelist_filter_for_youtube_channel = function(url) {
-    if (/channel/.test(url)) {
-      var get_channel = url.match(/channel=([^]*)/)[1];
+    if (/channel=/.test(url)) {
+      var yt_channel = url.match(/channel=([^]*)/)[1];
     } else {
-      var get_channel = url.split('/').pop();
+      var yt_channel = url.split('/').pop();
     }
-    var filter = '@@||youtube.com/*' + get_channel + '$document';
-    return add_custom_filter(filter);
+    if (yt_channel) {
+        var filter = '@@||youtube.com/*' + yt_channel + '$document';
+        return add_custom_filter(filter);
+    }
   }
 
   // Inputs: options object containing:
