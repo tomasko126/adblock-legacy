e9b7e5c4affbb9f6cdb53f37c553b10ffd9e7c8b
BrentM <brent.montrose@gmail.com>
2015-11-19 09:37:58 -0700

    format and remove debug statements

--- adblock_safari_beforeload.js
+++ adblock_safari_beforeload.js
@@ -1,22 +1,21 @@
 // Content script when Safari "beforeLoad" API is used
-console.log("beforeload");
-		adblock_begin({
-		  startPurger: function() {
-		    document.addEventListener("beforeload", beforeLoadHandler, true);
-		  },
-		  stopPurger: function() {
-		    document.removeEventListener("beforeload", beforeLoadHandler, true);
-		  },
-		  handleHiding: function(data) {
-		    if (data.hiding)
-		      block_list_via_css(data.selectors);
-		  },
-		  success: function() {
-		    onReady(function() { blockBackgroundImageAd(); });
+adblock_begin({
+  startPurger: function() {
+    document.addEventListener("beforeload", beforeLoadHandler, true);
+  },
+  stopPurger: function() {
+    document.removeEventListener("beforeload", beforeLoadHandler, true);
+  },
+  handleHiding: function(data) {
+    if (data.hiding)
+      block_list_via_css(data.selectors);
+  },
+  success: function() {
+    onReady(function() { blockBackgroundImageAd(); });
 
-		    // Add entries to right click menu of non-whitelisted pages.
-		    window.addEventListener("contextmenu", function(event) {
-		      safari.self.tab.setContextMenuEventUserInfo(event, true);
-		    }, false);
-		  }
-		});
+    // Add entries to right click menu of non-whitelisted pages.
+    window.addEventListener("contextmenu", function(event) {
+      safari.self.tab.setContextMenuEventUserInfo(event, true);
+    }, false);
+  }
+});
--- adblock_safari_contentblocking.js
+++ adblock_safari_contentblocking.js
@@ -1,19 +1,18 @@
 // Content script when Safari content blocking API is used
-console.log("content blocking");
-		adblock_begin({
-		  startPurger: function() {
-
-		  },
-		  stopPurger: function() {
-
-		  },
-		  handleHiding: function(data) {
-
-		  },
-		  success: function() {
-		    // Add entries to right click menu of non-whitelisted pages.
-		    window.addEventListener("contextmenu", function(event) {
-		      safari.self.tab.setContextMenuEventUserInfo(event, true);
-		    }, false);
-		  }
-		});
+adblock_begin({
+	startPurger: function() {
+		// Do nothing...
+	},
+	stopPurger: function() {
+		// Do nothing...
+	},
+	handleHiding: function(data) {
+		// Do nothing...
+	},
+	success: function() {
+		// Add entries to right click menu of non-whitelisted pages.
+		window.addEventListener("contextmenu", function(event) {
+			safari.self.tab.setContextMenuEventUserInfo(event, true);
+		}, false);
+	}
+});
