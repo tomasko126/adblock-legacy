97da292d24d2082cb35fdea59d0edee8f8e3c2fc
Brent Montrose <brent@getadblock.com>
2016-02-12 11:13:24 -0600

    update options / general tab

--- options/general.js
+++ options/general.js
@@ -10,6 +10,30 @@ catch(ex) {}
 // user's saved settings.
 $(function() {
 
+  function init_picreplacement() {
+    BGcall("picreplacement_show_on_options_page", function(show) {
+      var p = $("#picreplacement");
+      p.toggle(show);
+      p.find("[i18n]").each(function() {
+        $(this).text(picreplacement.translate($(this).attr("i18n")));
+      });
+      p.find("a").prop("href", picreplacement.translate("the_url"));
+    });
+  }
+  init_picreplacement();
+  // Labels fall off on tab change for some reason: redo them.
+  $("#tabpages").bind("tabsshow", init_picreplacement);
+  // Don't use standard enable_ machinery: this is too complicated.
+  BGcall("picreplacement_is_happening", function(enabled) {
+    var cb = $("#picreplacement").find(":checkbox");
+    cb.
+      attr("checked", enabled).
+      change(function() {
+        var is_enabled = $(this).is(':checked');
+        BGcall("set_setting", "do_picreplacement", is_enabled);
+      });
+  });
+
   BGcall("get_subscriptions_minus_text", function(subs) {
     //if the user is currently subscribed to AA
     //then 'check' the acceptable ads button.
